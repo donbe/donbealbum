d7eb33b3e5146dd0e8dcc1473d75bef2a29f41e7
donbe <donbe520@gmail.com>
2020-09-01 11:40:13 +0800

    init

---
+++ donbealbum/donbealbum/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  donbealbum
+//
+//  Created by donbe on 2020/8/31.
+//  Copyright © 2020 donbe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ donbealbum/donbealbum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  donbealbum
+//
+//  Created by donbe on 2020/8/31.
+//  Copyright © 2020 donbe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
